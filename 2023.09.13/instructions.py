010dc984a816dac77ff49594aabb42df0a8e6322
Szabó Péter <szabo.peter@students.jedlik.eu>
2023-09-13 11:37:14 +0200

    2023.09.13

--- 2023.09.13/instructions.py
+++ 2023.09.13/instructions.py
@@ -10,3 +10,44 @@ def szamHuzas(maxertek):
 
 def osszeadas(x, y):
     return x + y
+
+def kerekites(x: float):
+    return int(x + 0.5)
+
+def alapMuvelet(a, muvelet, b):
+    match muvelet:
+
+        case '+':
+            return a + b
+        
+        case '-':
+            return a - b
+        
+        case '*':
+            return a * b
+        
+        case '/':
+            return a / b
+        
+        case _:
+            return None
+        
+def egseszSzamBekeres(szoveg: str) -> int:
+    s = ''
+    while not s.isdecimal():
+        s = input(szoveg)
+    return int(s)
+
+def egseszSzamBekeres2(min, max, szoveg: str) -> int:
+    s = ''
+    while not s.isdecimal() or min > int(s) or int(s) > max:
+        s = input(szoveg)
+    return int(s)
+    
+
+    """
+    szam = min - 1
+    while not min <= szam <= max:
+        szam = egseszSzamBekeres(szoveg)
+    return szam
+    """
--- 2023.09.13/main.py
+++ 2023.09.13/main.py
@@ -1,6 +1,18 @@
 import os
 from instructions import *
-
+"""
 print(szamHuzas1_90())
 print(szamHuzas(45))
-print(12, 34)
+print(osszeadas(12, 34))
+print(alapMuvelet(3, '+', 4))
+print(alapMuvelet(3, '-', 4))
+print(alapMuvelet(3, '*', 4))
+print(alapMuvelet(3, '/', 4))
+
+
+f = egseszSzamBekeres("Kérem adjon meg egy számot: ")
+print(f)
+"""
+
+f =egseszSzamBekeres2(1, 12, "Kérem adja meg a hónap sorszámát: ")
+print(f)
